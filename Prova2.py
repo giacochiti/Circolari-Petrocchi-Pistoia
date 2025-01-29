60c9fd9cb052b135e826593cc891a99c4a0bfe33
giacochiti <giaco.chiti@gmail.com>
2025-01-29 14:23:15 +0100

    Update Prova2.py

--- Prova2.py
+++ Prova2.py
@@ -2,9 +2,9 @@ import requests
 import os
 
 # Sostituisci con il token del tuo bot
-TOKEN = 7305004967:AAGe1tySkfUANi9yp0Jh2uBNAJeWwHUG2SI # Aggiungi qui il token del tuo bot
+TOKEN = "7305004967:AAGe1tySkfUANi9yp0Jh2uBNAJeWwHUG2SI" # Aggiungi qui il token del tuo bot
 # Sostituisci con il chat ID del tuo account o gruppo
-CHAT_ID = 1885923992  # Aggiungi qui il chat ID
+CHAT_ID = "1885923992"  # Aggiungi qui il chat ID
 
 def send_telegram_message(message):
     """Invia un messaggio su Telegram"""
