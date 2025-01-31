a089969476b2364a7d4c59e84e56f737f4d96091
giacochiti <giaco.chiti@gmail.com>
2025-01-31 18:15:51 +0100

    Update check_circolari.py

--- check_circolari.py
+++ check_circolari.py
@@ -1,38 +1,42 @@
-# Percorso del file
-file_path = 'last_circular.txt'
+import requests
+from bs4 import BeautifulSoup
 
-# Funzione per leggere il numero dal file
-def read_last_circular():
-    try:
-        with open(file_path, 'r') as file:
-            content = file.read().strip()
-            # Estrai il numero dalla stringa
-            number = int(content)
-            return number
-    except FileNotFoundError:
-        print("Il file last_circular.txt non è stato trovato.")
-        return None
-    except ValueError:
-        print("Il contenuto del file non è un numero valido.")
-        return None
+# Configurazione Telegram
+chat_id = '1885923992'  # Il tuo ID
+token = '7305004967:AAGe1tySkfUANi9yp0Jh2uBNAJeWwHUG2SI'  # Il token del bot
 
-# Funzione per salvare il nuovo numero nel file
-def save_new_circular(number):
-    with open(file_path, 'w') as file:
-        file.write(f"{number}")
+# URL della pagina delle circolari
+url = 'https://liceoartisticopistoia.edu.it/circolari/'
+
+# Funzione per inviare un messaggio su Telegram tramite l'API
+def send_telegram_message(message):
+    api_url = f"https://api.telegram.org/bot{token}/sendMessage"
+    payload = {
+        'chat_id': chat_id,
+        'text': message
+    }
+    response = requests.post(api_url, data=payload)
+    if response.status_code == 200:
+        print("Messaggio inviato con successo!")
+    else:
+        print(f"Errore nell'invio del messaggio: {response.status_code}")
+
+# Funzione per ottenere l'ultima circolare
+def get_latest_circular():
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Trova il primo elemento della circolare più recente
+    latest_circular_element = soup.find('div', class_='wpdm-link-tpl')
+    circular_title = latest_circular_element.find('strong', class_='ptitle').text.strip()
+    
+    return circular_title
 
 # Main
 if __name__ == "__main__":
-    # Leggi il numero dal file
-    last_number = read_last_circular()
+    # Ottieni l'ultima circolare dal sito
+    circular_title = get_latest_circular()
     
-    if last_number is not None:
-        # Incrementa il numero di uno
-        new_number = last_number + 1
-        
-        # Salva il nuovo numero nel file
-        save_new_circular(new_number)
-        
-        print(f"Numero incrementato e salvato: {new_number}")
-    else:
-        print("Non è stato possibile leggere il contenuto del file.")
+    # Invia il titolo della circolare tramite Telegram
+    message = f"Ultima circolare pubblicata: {circular_title}"
+    send_telegram_message(message)
