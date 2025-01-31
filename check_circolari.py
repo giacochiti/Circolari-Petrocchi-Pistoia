c99b6a65282d5d918bca9d7776b54c1cf30b9b44
giacochiti <giaco.chiti@gmail.com>
2025-01-31 17:15:25 +0100

    Update check_circolari.py

--- check_circolari.py
+++ check_circolari.py
@@ -1,8 +1,12 @@
 import requests
+from bs4 import BeautifulSoup
 
 # Configurazione Telegram
 chat_id = '1885923992'  # Il tuo ID
 token = '7305004967:AAGe1tySkfUANi9yp0Jh2uBNAJeWwHUG2SI'  # Il token del bot
+
+# URL della pagina delle circolari
+url = 'https://liceoartisticopistoia.edu.it/circolari/'
 
 # Percorso del file
 file_path = 'last_circular.txt'
@@ -30,10 +34,34 @@ def read_last_circular():
         print("Il file last_circular.txt non è stato trovato.")
         return None
 
+# Funzione per ottenere l'ultima circolare
+def get_latest_circular():
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Trova il primo elemento della circolare più recente
+    latest_circular_element = soup.find('div', class_='wpdm-link-tpl')
+    if latest_circular_element is None:
+        print("Impossibile trovare l'elemento della circolare più recente.")
+        return None, None
+    
+    circular_title = latest_circular_element.find('strong', class_='ptitle').text.strip()
+    circular_link = latest_circular_element.find('a')['href']
+    
+    return circular_title, circular_link
+
 # Main
 if __name__ == "__main__":
+    # Leggi il contenuto del file last_circular.txt
     circular_content = read_last_circular()
     if circular_content:
-        send_telegram_message(circular_content)
+        send_telegram_message(f"Contenuto di last_circular.txt:\n{circular_content}")
     else:
         print("Non è stato possibile leggere il contenuto del file.")
+    
+    # Ottieni l'ultima circolare dal sito
+    circular_title, circular_link = get_latest_circular()
+    if circular_title:
+        send_telegram_message(f"Nuova circolare pubblicata:\nTitolo: {circular_title}\nLink: {circular_link}")
+    else:
+        print("Non è stato possibile recuperare l'ultima circolare dal sito.")
